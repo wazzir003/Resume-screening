eef30bc60fba6b23dd4ed34e7923e8efef730836
wazzir003 <abhinashsingh4430@gmail.com>
2025-05-30 21:02:48 +0530

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,8 @@
+import PyPDF2
+
+def extract_text_from_pdf(file):
+    reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() or ""
+    return text
